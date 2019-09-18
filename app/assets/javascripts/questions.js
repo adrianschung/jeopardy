0fdcf5d4786bdc553822308d912d7b920b4e2c4d
adrianschung <adrianschung@gmail.com>
2019-09-18 09:20:15 -0400

    Add user authentication

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,5 +1,4 @@
 $(document).on('turbolinks:load', function() {
-  console.log('loaded')
   $('#question_answer').select2({
     placeholder: "What is",
     allowClear: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_200511) do
+ActiveRecord::Schema.define(version: 2019_09_18_125233) do
 
   create_table "answered_questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "game_id"
@@ -51,6 +51,14 @@ ActiveRecord::Schema.define(version: 2019_09_17_200511) do
     t.integer "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
 end
